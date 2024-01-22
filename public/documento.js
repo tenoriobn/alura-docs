e9e847b83d45205af9c521bb2706530ed31087dd
Bruno Tenório <tenoriobn@gmail.com>
2024-01-22 17:41:47 -0300

    Do servidor para os clientes

--- public/documento.js
+++ public/documento.js
@@ -5,3 +5,7 @@ const textoEditor = document.getElementById("editor-texto");
 textoEditor.addEventListener("keyup", () => {
   socket.emit("texto_editor", textoEditor.value);
 });
+
+socket.on("texto_editor_clientes", (texto) => {
+  textoEditor.value = texto;
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -4,6 +4,6 @@ io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
 
   socket.on("texto_editor", (texto) => {
-    console.log(texto)
+    socket.broadcast.emit("texto_editor_clientes", texto);
   })
 })
