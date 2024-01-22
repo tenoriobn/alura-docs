5ea3f27523e11aae7971853cf84194c681b5eab6
Bruno Tenório <tenoriobn@gmail.com>
2024-01-22 17:12:03 -0300

    Emitindo eventos

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,5 +1,9 @@
 import io from "./servidor.js"
 
 io.on("connection", (socket) => {
-  console.log("Um cliente se conectou! ID:", socket.id)
+  console.log("Um cliente se conectou! ID:", socket.id);
+
+  socket.on("texto_editor", (texto) => {
+    console.log(texto)
+  })
 })
